5c9d8a54ced9614caae9cfaca60fe592df86636a
Brahim tuijine <brahimtuijine220@gmail.com>
2024-11-03 23:50:28 +0100

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by MacBook on 03/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
