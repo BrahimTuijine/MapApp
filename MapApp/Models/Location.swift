924a87d56e4e2b834a4fe5e22d44d795edbc7b19
Brahim tuijine <brahimtuijine220@gmail.com>
2024-11-04 00:07:33 +0100

    add Location model

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by MacBook on 04/11/2024.
+//
+
+import Foundation
+import MapKit
+
+
+struct Location {
+    let name: String
+    let description: String
+    let cityName: String
+    let link: String
+    let images: [String]
+    let coordinates: CLLocationCoordinate2D
+    
+}
