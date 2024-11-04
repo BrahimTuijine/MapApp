c642b5347b0e7bb7f012228ca54008ea835a11ae
Brahim tuijine <brahimtuijine220@gmail.com>
2024-11-04 09:59:23 +0100

    add Map from MapKit

--- MapApp/Views/LocationsView.swift
+++ MapApp/Views/LocationsView.swift
@@ -6,16 +6,23 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationsView: View {
     
     @EnvironmentObject var vm : LocationsViewModel
     
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 41.8902, longitude: 12.4922),
+            span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        )
+    )
+    
     var body: some View {
-        List {
-            ForEach(vm.locations) { location in
-                Text(location.name )
-            }
+        ZStack {
+            Map(position: $position)
+                .ignoresSafeArea()
         }
     }
 }
