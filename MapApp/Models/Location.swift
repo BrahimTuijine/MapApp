2f6f388ae8ae786661fd04afab8a6eaa676e27d1
Brahim tuijine <brahimtuijine220@gmail.com>
2024-11-05 10:58:39 +0100

    sisable the title animation of the list.

--- MapApp/Models/Location.swift
+++ MapApp/Models/Location.swift
@@ -9,7 +9,11 @@ import Foundation
 import MapKit
 
 
-struct Location : Identifiable {
+struct Location : Identifiable, Equatable {
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
+    
     let id: String = UUID().uuidString
     let name: String
     let cityName: String
